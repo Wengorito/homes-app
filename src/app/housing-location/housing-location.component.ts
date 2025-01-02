44826abce0cbbc85205e52b58f28f5b052861162
Grzegorz Kowalski <grko@bws.dk>
2025-01-02 17:20:56 +0100

    Add Activated Route (item id in the details page).

--- src/app/housing-location/housing-location.component.ts
+++ src/app/housing-location/housing-location.component.ts
@@ -13,7 +13,7 @@ import { RouterModule } from '@angular/router';
     alt="Exterior photo of {{housingLocation.name}}">
     <h2 class="listing-heading">{{housingLocation.name}}</h2>
     <p class="listing-location">{{housingLocation.city}},{{housingLocation.state}}</p>
-    <a routerLink="details">Learn More</a>
+    <a [routerLink]="['/details', housingLocation.id]">Learn More</a>
   </section>
   `,
   styleUrls: ['./housing-location.component.css']
